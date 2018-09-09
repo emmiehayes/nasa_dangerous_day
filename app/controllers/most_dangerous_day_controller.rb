04785a723c4339817496c7655e15f0ccaeb78bb2
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-09 12:42:54 -0600

    instantiate SearchPresenter in controller, build private method to narrow params

--- app/controllers/most_dangerous_day_controller.rb
+++ app/controllers/most_dangerous_day_controller.rb
@@ -1,4 +1,11 @@
 class MostDangerousDayController < ApplicationController
   def index
+    @presenter = SearchPresenter.new(search_params)
+  end
+
+  private 
+
+  def search_params 
+    params.permit(:start_date, :end_date)
   end
 end
