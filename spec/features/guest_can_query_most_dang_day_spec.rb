2062528eaaa203bc18dd5233b9b99ec4844960b5
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-09 15:32:30 -0600

    hedious code to present most dangerous day

--- spec/features/guest_can_query_most_dang_day_spec.rb
+++ spec/features/guest_can_query_most_dang_day_spec.rb
@@ -7,9 +7,9 @@ describe 'a guest user visiting the root page' do
 
   within(".inner-nav") do 
     # And I enter "2018-01-01" into the start date
-    fill_in 'start_date', with: "2018-01-01"
+    fill_in 'start_date', with: "2018-09-08"
     # And I enter "2018-01-07" into the end date
-    fill_in 'end_date', with: "2018-01-07"
+    fill_in 'end_date', with: "2018-09-14"
     # And I click "Determine Most Dangerous Day"
     click_on 'Determine Most Dangerous Day'
   end
@@ -19,31 +19,24 @@ describe 'a guest user visiting the root page' do
     # And I should see a heading for "Most Dangerous Day"
     expect(page).to have_content("Most Dangerous Day")
     # And I should see "January 1, 2018 - January 7, 2018"
-    expect(page).to have_content("Date Range Submitted: January 1, 2018 - January 7, 2018")
+    expect(page).to have_content("Date Range Submitted: September 8, 2018 - September 14, 2018")
     # And I should see a heading for the most dangerous day in that range "January 1, 2018"
-    expect(page).to have_content("Most Dangerous Day: January 1, 2018")
+    expect(page).to have_content("Most Dangerous Day: September 11, 2018")
     # And I should see 3 asteroids in that list
-    expect(page).to have_css(".asteriods", count: 3)
+    expect(page).to have_css(".asteriods", count: 2)
 
     within(first(".asteriod")) do
       # And I should see "Name: (2014 KT76)"
-      expect(page).to have_content("Name: (2014 KT76)")
+      expect(page).to have_content("Name: (2008 KZ5)")
       # And I should see "NEO Reference ID: 3672906"
-      expect(page).to have_content("NEO Reference ID: 3672906")
+      expect(page).to have_content("NEO Reference ID: 3412889")
     end
 
     within(second(".asteriod")) do
       # And I should see "Name: (2001 LD)"
-      expect(page).to have_content("Name: (2001 LD)")
+      expect(page).to have_content("Name: (2010 CO1)")
       # And I should see "NEO Reference ID: 3078262"
-      expect(page).to have_content("NEO Reference ID: 3078262")
+      expect(page).to have_content("NEO Reference ID: 3507718")
     end
-
-    within(third(".asteriod")) do
-      # And I should see "Name: (2017 YR1)"
-      expect(page).to have_content("Name: (2017 YR1)")
-      # And I should see "NEO Reference ID: 3794979"
-      expect(page).to have_content("NEO Reference ID: 3794979")    
-    end 
   end
 end 
